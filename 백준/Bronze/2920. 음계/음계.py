a034bf35d14933815bd8f1c41c50849869af861c
TwinKay <101968683+TwinKay@users.noreply.github.com>
2022-08-14 23:01:30 +0900

    [Bronze II] Title: 음계, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+ummgye = list(map(int, input().split()))
+if ummgye == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print('ascending')
+elif ummgye == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print('descending')
+else:
+    print('mixed')
