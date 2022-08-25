f2f8b5212c3aebc984e52b4c1b3ad02a053fa749
고금강 <101968683+TwinKay@users.noreply.github.com>
2022-08-25 17:46:28 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,35 @@
+import sys
+
+total = []
+for _ in range(int(sys.stdin.readline())):
+    x = sys.stdin.readline().strip()
+    if x[:2] == 'pu':
+        a,b = x.split()
+        total.append(int(b))
+    else:
+        if x == 'pop':
+            if total != []:
+                print(total.pop(0))
+            else:
+                print(-1)
+
+        elif x == 'size':
+            print(len(total))
+
+        elif x == 'empty':
+            if total == []:
+                print(1)
+            else:
+                print(0)
+
+        elif x == 'front':
+            if total != []:
+                print(total[0])
+            else:
+                print(-1)
+
+        else:
+            if total != []:
+                print(total[-1])
+            else:
+                print(-1)
