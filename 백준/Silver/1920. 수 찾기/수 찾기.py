2ff710a5a74d152174e5ad47f39301ed7356be91
고금강 <101968683+TwinKay@users.noreply.github.com>
2022-08-25 19:14:16 +0900

    [Silver IV] Title: 수 찾기, Time: 528 ms, Memory: 46824 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(sys.stdin.readline())
+main = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+case = list(map(int, sys.stdin.readline().split()))
+main.sort()
+
+def binary(main_list, case_i):
+    start = 0 ; end = n-1
+    while start <= end:
+        mid = (start+end)//2
+        if main_list[mid] == case_i:
+            return 1
+        elif main_list[mid] > case_i:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return 0
+
+for i in range(m):
+    if binary(main, case[i]) == 1:
+        print(1)
+    else:
+        print(0)
