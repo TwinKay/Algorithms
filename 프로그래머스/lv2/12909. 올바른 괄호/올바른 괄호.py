1bf8a80bfe54c7414cf4e9883abba15d22719e49
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-01-19 11:50:33 +0900

    [level 2] Title: 올바른 괄호, Time: 7.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    l = 0
+    r = 0
+    ans = True
+    for i in s:
+        if i == '(':
+            l += 1
+        else:
+            r += 1
+        if l<r:
+            ans = False
+            break
+    if l != r:
+        ans = False
+        
+    return ans
