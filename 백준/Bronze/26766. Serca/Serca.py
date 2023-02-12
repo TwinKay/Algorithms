9394cd530a1aaa155c003bb5cf8073b74aba89a0
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-02-12 23:03:42 +0900

    [Bronze V] Title: Serca, Time: 160 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+for _ in range(n):
+    print(''' @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     ''')
