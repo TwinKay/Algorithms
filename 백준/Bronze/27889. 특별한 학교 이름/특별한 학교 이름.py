ee3d61676e7392ef591aa45ac705bb9bb811310e
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-03-31 23:17:27 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,3 @@
+import sys
+dic = {'NLCS':'North London Collegiate School','BHA':'Branksome Hall Asia','KIS':'Korea International School','SJA':'St. Johnsbury Academy'}
+print(dic[sys.stdin.readline().rstrip()])
