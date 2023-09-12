e5a19b69dfa3f891756dde2e5b09c934823ca2e2
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-09-12 09:23:49 +0900

    [Silver V] Title: 집합, Time: 3720 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,40 +1,31 @@
 import sys
 
-result = set()
-all_set = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
-empty_set = set()
+s = set()
 
-for _ in range(int(sys.stdin.readline())):
-    x = sys.stdin.readline().strip()
+n = int(sys.stdin.readline())
 
-    if x == 'all':
-        result = all_set
+for _ in range(n):
+    a = sys.stdin.readline().rstrip()
 
-
-    elif x == 'empty':
-        result = empty_set
-
-
+    if a == 'all':
+        s = set(range(1,21))
+    elif a == 'empty':
+        s = set()
     else:
-        a, b = x.split()
+        c, b = a.split()
         b = int(b)
 
-        if a == 'add':
-            if b not in result:
-                result.add(b)
-
-        elif a == 'remove':
-            if b in result:
-                result.remove(b)
-
-        elif a == 'check':
-            if b in result:
+        if c == 'add':
+            s.add(b)
+        elif c == 'remove':
+            s.discard(b)
+        elif c == 'check':
+            if b in s:
                 print(1)
             else:
                 print(0)
-
-        elif a == 'toggle':
-            if b in result:
-                result.remove(b)
+        elif c == 'toggle':
+            if b in s:
+                s.discard(b)
             else:
-                result.add(b)
+                s.add(b)
