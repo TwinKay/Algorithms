6c0d8c73733f874bd350dd4548841198ae1b9d88
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-09-21 15:08:14 +0900

    [Silver III] Title: N과 M (5), Time: 212 ms, Memory: 42180 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,27 @@
+import sys
+
+def make_num_lsts(lst):
+    temp = []
+    for i in num_range:
+        if i not in lst:
+            temp.append(lst+[i])
+    return temp
+
+n,m = map(int, sys.stdin.readline().split())
+
+num_range = list(map(int, sys.stdin.readline().split()))
+num_range.sort()
+res = []
+for i in num_range:
+    res.append([i])
+
+temp = []
+for rp in range(m-1):
+    for i in res:
+        temp.extend(make_num_lsts(i))
+    res = temp
+    temp = []
+
+for i in res:
+    i = list(map(str,i))
+    print(' '.join(i))
