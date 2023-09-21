be8255da6e80e97047594cd0e9a060abb2915e4d
고금강 <101968683+TwinKay@users.noreply.github.com>
2023-09-21 14:49:11 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,27 @@
+import sys
+# from collections import deque
+
+def make_num_lsts(lst):
+    temp = []
+    for i in num_range:
+        if i > lst[-1]:
+            temp.append(lst+[i])
+    return temp
+
+n,m = map(int, sys.stdin.readline().split())
+
+num_range = list(range(1,n+1))
+res = []
+for i in num_range:
+    res.append([i])
+
+temp = []
+for rp in range(m-1):
+    for i in res:
+        temp.extend(make_num_lsts(i))
+    res = temp
+    temp = []
+
+for i in res:
+    i = list(map(str,i))
+    print(' '.join(i))
