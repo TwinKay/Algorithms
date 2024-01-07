ba2b8d63dfe9ff654824378f6bb382fb8f2d0bc1
고금강 <101968683+TwinKay@users.noreply.github.com>
2024-01-07 14:21:43 +0900

    [Gold IV] Title: 서강그라운드, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -0,0 +1,43 @@
+import heapq as hq
+import sys
+# m : 수색범위
+v,m,e = map(int, sys.stdin.readline().split())
+items = list(map(int, sys.stdin.readline().split()))
+
+graph = []
+for _ in range(v+1):
+    graph.append([])
+
+for _ in range(e):
+    a,b,c = map(int, sys.stdin.readline().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+
+def dijkstra(start):
+    dis = [1e9]*(v+1)
+    heap = []
+    hq.heappush(heap, [0,start])
+    dis[start] = 0
+
+    while heap:
+        dist, point = hq.heappop(heap)
+
+        if dis[point] < dist:
+            continue
+
+        for i in graph[point]:
+            cost = dist + i[1]
+            if cost < dis[i[0]]:
+                dis[i[0]] = cost
+                hq.heappush(heap, [dis[i[0]], i[0]])
+    
+    temp = 0
+    for i in range(1,v+1):
+        if dis[i] <= m:
+            temp += items[i-1]
+    return temp
+
+total = []
+for i in range(1,v+1):
+    total.append(dijkstra(i))
+print(max(total))
