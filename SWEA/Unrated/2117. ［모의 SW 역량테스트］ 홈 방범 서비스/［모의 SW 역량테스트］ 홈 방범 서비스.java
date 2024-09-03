42b9174de1047df50b9730049dc48f5793680549
고금강 <101968683+TwinKay@users.noreply.github.com>
2024-09-03 16:13:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 176 ms, Memory: 24,176 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -10,8 +10,8 @@ public class Solution {
     static StringTokenizer token;
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M, maxK, cost;
-    static List<int[]> HouseIdx;
+    static int N, M, numHouse, maxK, cost;
+    static List<int[]> houseIdx;
 
 
     public static void main(String[] args) throws IOException {
@@ -21,31 +21,42 @@ public class Solution {
             N = Integer.parseInt(token.nextToken());
             M = Integer.parseInt(token.nextToken());
 
-            HouseIdx = new ArrayList<>();
+            numHouse = 0;
+            houseIdx = new ArrayList<>();
 
             for (int i = 0; i < N; i++) {
                 token = new StringTokenizer(input.readLine());
                 for (int j = 0; j < N; j++) {
                     int temp = Integer.parseInt(token.nextToken());
                     if (temp == 1) {
-                        HouseIdx.add(new int[]{j, i});
+                        houseIdx.add(new int[]{j, i});
+                        numHouse++;
                     }
                 }
             }
 
             // 미리 최대 k 계산
             maxK = 1;
-            while (maxK * maxK + (maxK - 1) * (maxK - 1) <= HouseIdx.size() * M) {
+            while (maxK * maxK + (maxK - 1) * (maxK - 1) <= numHouse * M) {
                 maxK++;
             }
 
             int res = 0;
-            for (int k=1; k <= maxK; k++) {
+//            for (int k=1; k <= maxK; k++) {
+//                cost = k*k+(k-1)*(k-1);
+//                for (int i = 0; i < N; i++) {
+//                    for (int j = 0; j < N; j++) {
+//                        res = Math.max(res, cntHouse(i, j, k));
+//
+//                    }
+//                }
+//            }
+            for (int k=maxK; k >= 1; k--) {
                 cost = k*k+(k-1)*(k-1);
                 for (int i = 0; i < N; i++) {
                     for (int j = 0; j < N; j++) {
                         res = Math.max(res, cntHouse(i, j, k));
-
+                        if (res==numHouse) break;
                     }
                 }
             }
@@ -55,7 +66,7 @@ public class Solution {
     }
     public static int cntHouse(int x, int y, int k){
         int cnt = 0;
-        for (int[] idx : HouseIdx) {
+        for (int[] idx : houseIdx) {
             int hx = idx[0];
             int hy = idx[1];
             if (Math.abs(hx - x)+Math.abs(hy - y)<=k-1) {
