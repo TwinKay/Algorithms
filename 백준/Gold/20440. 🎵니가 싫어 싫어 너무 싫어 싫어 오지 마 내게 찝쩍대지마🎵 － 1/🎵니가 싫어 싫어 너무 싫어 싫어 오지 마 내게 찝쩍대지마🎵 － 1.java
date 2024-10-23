f56258ae1bab7856d0e032715e7cf0632b11c656
고금강 <101968683+TwinKay@users.noreply.github.com>
2024-10-23 17:01:05 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 1572 ms, Memory: 389240 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -12,51 +11,39 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
 
     static int N;
-    static Map<Integer,Integer> map;
+    static Map<Integer, Integer> map;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(input.readLine());
-        map = new HashMap<>();
+        map = new TreeMap<>();
         for (int i = 0; i < N; i++) {
             token = new StringTokenizer(input.readLine());
             int start = Integer.parseInt(token.nextToken());
             int end = Integer.parseInt(token.nextToken());
-            map.put(start, map.getOrDefault(start, 0)+1);
-            map.put(end, map.getOrDefault(end, 0)-1);
-        }
-        int[][] info = new int[map.size()][2];
-        Object[] keys = map.keySet().toArray();
-        for (int i = 0; i < info.length; i++) {
-            info[i][0] = (int) keys[i];
-            info[i][1] = map.get(info[i][0]);
+            map.put(start, map.getOrDefault(start, 0) + 1);
+            map.put(end, map.getOrDefault(end, 0) - 1);
         }
 
-        Arrays.sort(info, (o1,o2) ->{
-            return o1[0]-o2[0];
-        });
-        int[] imos = new int[info.length];
-        imos[0] = info[0][1];
-        int max = imos[0];
-        for (int i = 1; i < info.length; i++) {
-            imos[i] = imos[i-1] + info[i][1];
-            max = Math.max(max, imos[i]);
-        }
+        int max = 0;
+        int imos = 0;
         int resStart = -1;
         int resEnd = -1;
-        for (int i = 0; i < imos.length; i++) {
-            if(imos[i]==max){
-                if(resStart==-1){
-                    resStart = i;
-                }
-            }else{
-                if(resStart!=-1){
-                    resEnd = i;
-                    break;
-                }
+        boolean inMaxRange = false;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            imos += entry.getValue();
+            if (imos > max) {
+                max = imos;
+                resStart = entry.getKey();
+                inMaxRange = true;
+            } else if (inMaxRange && imos < max) {
+                resEnd = entry.getKey();
+                inMaxRange = false;
             }
         }
+
         sb.append(max).append("\n");
-        sb.append(info[resStart][0]).append(" ").append(info[resEnd][0]);
+        sb.append(resStart).append(" ").append(resEnd);
         System.out.println(sb);
     }
 }
