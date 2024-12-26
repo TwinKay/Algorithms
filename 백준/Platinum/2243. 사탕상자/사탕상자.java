4fdac3e8ab35a0cf9ce134a772f5524536cd7aed
고금강 <101968683+TwinKay@users.noreply.github.com>
2024-12-26 15:23:25 +0900

    [Platinum V] Title: 사탕상자, Time: 648 ms, Memory: 56920 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.java
+++ 백준/Platinum/2243. 사탕상자/사탕상자.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -21,23 +22,7 @@ public class Main {
             int a = Integer.parseInt(token.nextToken());
             int b = Integer.parseInt(token.nextToken());
             if (a==1){
-                int tempIdx = binarySearchSum(1,N,b);
-                if (tempIdx<0){
-                    tempIdx = -tempIdx-1;
-                }
-                int tempVal = sum(1,N,1,1,tempIdx);
-                int low = 1;
-                int high = tempIdx;
-
-                while (low < high) {
-                    int mid = (low + high) >>> 1;
-                    if (sum(1, N, 1, 1, mid) == tempVal) {
-                        high = mid;
-                    } else {
-                        low = mid + 1;
-                    }
-                }
-                tempIdx = low;
+                int tempIdx = binarySearchRank(1, N, b);
                 sb.append(tempIdx).append("\n");
                 update(1, N, 1, tempIdx, -1);
             }else{
@@ -47,23 +32,25 @@ public class Main {
         }
         System.out.println(sb);
     }
-    private static int binarySearchSum(int fromIndex, int toIndex, int key) {
+    private static int binarySearchRank(int fromIndex, int toIndex, int key) {
         int low = fromIndex;
         int high = toIndex;
+        int result = -1;
 
         while (low <= high) {
             int mid = (low + high) >>> 1;
-            int midVal = sum(1,N,1,1,mid);
+            int midVal = sum(1, N, 1, 1, mid);
 
-            if (midVal < key)
-                low = mid + 1;
-            else if (midVal > key)
+            if (midVal >= key) {
+                result = mid;
                 high = mid - 1;
-            else
-                return mid;
+            } else {
+                low = mid + 1;
+            }
         }
-        return -(low + 1);
+        return result;
     }
+
 
     public static int sum(int start, int end, int node, int left, int right){
         if (left>end || right<start){
