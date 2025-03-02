9213b06610be21d5c4edd9db5278850c7ef7ac8a
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-03-02 22:12:34 +0900

    [Bronze IV] Title: Time to Decompress, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/17010. Time to Decompress/Time to Decompress.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer token;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(input.readLine());
+        for (int t = 1; t <= T; t++) {
+            token = new StringTokenizer(input.readLine());
+            int n = Integer.parseInt(token.nextToken());
+            char c = token.nextToken().charAt(0);
+            for (int i = 0; i < n; i++) {
+                sb.append(c);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
