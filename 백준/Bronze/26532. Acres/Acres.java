3e89a3bda87caccb7b95593de235847f384976dd
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-04-03 21:28:02 +0900

    [Bronze IV] Title: Acres, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer token;
+
+    public static void main(String[] args) throws IOException {
+        token = new StringTokenizer(input.readLine());
+        int a = Integer.parseInt(token.nextToken());
+        int b = Integer.parseInt(token.nextToken());
+        double acres = a*b / 4840.0;
+        System.out.println((int) Math.ceil(acres / 5.0));
+    }
+}
