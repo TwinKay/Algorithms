8a6e79a4348d386666b76c6bee3da83aade35b4c
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-06-04 20:42:27 +0900

    [Bronze IV] Title: Cornhole, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27855. Cornhole/Cornhole.py
@@ -0,0 +1,13 @@
+import sys
+
+h1, b1 = map(int, sys.stdin.readline().split())
+h2, b2 = map(int, sys.stdin.readline().split())
+s1 = h1 * 3 + b1
+s2 = h2 * 3 + b2
+
+if s1 == s2:
+    print("NO SCORE")
+elif s1 > s2:
+    print(1, s1 - s2)
+else:
+    print(2, s2 - s1)
