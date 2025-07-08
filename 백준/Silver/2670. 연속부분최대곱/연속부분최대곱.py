e857c46a01b57734df56e6d945f40d99653d1767
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-09 01:29:30 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = [float(sys.stdin.readline()) for _ in range(N)]
+
+dp = [0.0] * N
+dp[0] = arr[0]
+max_val = dp[0]
+
+for i in range(1, N):
+    dp[i] = max(arr[i], dp[i-1] * arr[i])
+    max_val = max(max_val, dp[i])
+
+print(f"{max_val:.3f}")
