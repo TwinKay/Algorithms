6f1dc24426eb2ab8f7c1208da986010caa4de67c
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-08 21:56:07 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,14 +1,23 @@
-alpha = ["A","B","C","D","E","F","G","H",'I',"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-L = [2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9,9,9]
+import sys
 
-s = list(input())
-
-dic = {}
-for i in alpha:
-    dic[i]=L[alpha.index(i)]+1
-
-total = 0
-for i in s:
-    total += dic[i]
-    
-print(total)
+s = sys.stdin.readline().rstrip()
+res = 0
+for c in s:
+    asc = ord(c)-65
+    if asc<=2:
+        res += 3
+    elif asc<=5:
+        res += 4
+    elif asc<=8:
+        res += 5
+    elif asc<=11:
+        res += 6
+    elif asc<=14:
+        res += 7
+    elif asc<=18:
+        res += 8
+    elif asc<=21:
+        res += 9
+    else:
+        res += 10
+print(res)
