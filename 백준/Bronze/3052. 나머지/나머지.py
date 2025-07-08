9da9b259b9e1f33ce17a93f83601035d816d06b1
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-08 19:12:04 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,11 @@
-L = []
+import sys
 
-for i in range(10):
-    L.append(int(input())%42)
-
-L = list(set(L))
-print(len(L))
+arr = list(False for _ in range(42))
+for _ in range(10):
+    n = int(sys.stdin.readline())
+    arr[n%42] = True
+res = 0
+for bol in arr:
+    if bol:
+        res += 1
+print(res)
