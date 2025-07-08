3756377d6f49f1eb5497d4a309c2bce6236e3511
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-08 23:43:18 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,30 +1,43 @@
-m,n = map(int, input().split())
-board = []
-for i in range(m):
-    board.append(input())
+import sys
 
-result = []
-for i in range(m-7):
-    result.append(board[i:i+8])
+N,M = map(int, sys.stdin.readline().split())
+graph = []
+for i in range(N):
+    s = sys.stdin.readline().rstrip()
+    graph.append([])
+    for j in range(M):
+        graph[i].append(s[j])
 
-c = 0
-find_min = []
-for o in range(2):
-    for i in result:
-        for j in range(n-7):
-            for a,k in enumerate(i):
-                
-                if a%2 == o:
-                    for l in range(8):
-                        if k[j+l] != 'BWBWBWBW'[l]:
-                            c += 1
-                            
-                else:
-                     for l in range(8):
-                        if k[j+l] != 'WBWBWBWB'[l]:
-                            c += 1
-            find_min.append(c)
-            c = 0
+chess1 = []
+chess2 = []
+res = 100
+temp = True
+for i in range(8):
+    if temp:
+        chess1.append(["W","B","W","B","W","B","W","B"])
+        chess2.append(["B","W","B","W","B","W","B","W"])
+        temp = False
+    else:
+        chess1.append(["B","W","B","W","B","W","B","W"])
+        chess2.append(["W","B","W","B","W","B","W","B"])
+        temp = True
 
-
-print(min(find_min))
+for i in range(N-8+1):
+    for j in range(M-8+1):
+        cnt = 0
+        for y in range(i,i+8):
+            for x in range(j,j+8):
+                if graph[y][x] != chess1[y-i][x-j]:
+                    cnt += 1
+        if cnt<res:
+            res = cnt
+            
+        cnt = 0
+        for y in range(i,i+8):
+            for x in range(j,j+8):
+                if graph[y][x] != chess2[y-i][x-j]:
+                    cnt += 1
+        if cnt<res:
+            res = cnt
+            
+print(res)
