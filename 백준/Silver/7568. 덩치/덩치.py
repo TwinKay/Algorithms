f460eca540d1f3653fa818ebb9b8813c6a57a643
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-09 00:26:22 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,23 +1,14 @@
-n = int(input())
-dungchi = []
-result = []
+import sys
 
-for i in range(n):
-    a,b = map(int,input().split())
-    dungchi.append([a,b])
-
-
-for i in dungchi:
-    t = 1
-    for j in dungchi:
-        
-        if i[0]<j[0] and i[1]<j[1]:
-            t += 1
-    result.append(t)
-    
-for i,j in enumerate(result):
-    if i == n-1:
-        print(j)
-    
-    else:
-        print(j, end=' ')
+N = int(sys.stdin.readline())
+lst = []
+for _ in range(N):
+    lst.append(list(map(int, sys.stdin.readline().split())))
+res = []
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
+            cnt+=1
+    res.append(cnt+1)
+print(" ".join(map(str,res)))
