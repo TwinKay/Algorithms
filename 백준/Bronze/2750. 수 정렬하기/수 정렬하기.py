9ef960e13c6fb2069f42104e42c961d7ce920667
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-08 23:09:28 +0900

    [Bronze II] Title: 수 정렬하기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,9 @@
 import sys
 
-n = int(sys.stdin.readline())
-result = []
-for i in range(n):
-    result.append(int(sys.stdin.readline()))
-result.sort()
-
-for j in result:
-    print(j)
+N = int(sys.stdin.readline())
+lst = []
+for _ in range(N):
+    lst.append(int(sys.stdin.readline()))
+lst.sort()
+for i in lst:
+    print(i)
