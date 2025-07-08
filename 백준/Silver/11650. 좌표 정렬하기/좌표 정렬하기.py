31c0c1b282c3029298924295c28f8a278ba9a35e
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-09 00:19:42 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 356 ms, Memory: 61144 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,15 +1,9 @@
 import sys
 
-n = int(sys.stdin.readline())
-total = []
-
-for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    total.append([a, b])
-
-total.sort()
-
-for i in range(n):
-    print(total[i][0], total[i][1])
-
-# 이중 리스트 or 튜블 모두 sort로 자동 정렬 가능
+N = int(sys.stdin.readline())
+lst = []
+for _ in range(N):
+    lst.append(list(map(int, sys.stdin.readline().split())))
+lst = sorted(lst, key=lambda x: (x[0],x[1]))
+for idx in lst:
+    print(f'{idx[0]} {idx[1]}')
