b06aaf444bef1512ceeb760c1b80fc2698feedda
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-09 00:36:26 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,44 +1,36 @@
 import sys
 
 while True:
+    is_bal = True
     s = sys.stdin.readline().rstrip()
-    if s == '.':
+    if s == ".":
         break
+    
+    stack = []
+    for c in s:
+        if c in ["[","("]:
+            stack.append(c)
+        elif c == "]":
+            if len(stack) == 0:
+                is_bal=False
+                break
+            if (stack[-1]=="["):
+                stack.pop()
+            else:
+                is_bal=False
+                break
+                
+        elif c == ")":
+            if len(stack) == 0:
+                is_bal=False
+                break
+            if (stack[-1]=="("):
+                stack.pop()
+            else:
+                is_bal=False
+                break
+                
+    if len(stack)==0 and is_bal:
+        print("yes")
     else:
-        count_small = 0
-        count_big = 0
-        pair = []
-
-        for i in s:
-            if i == '(':
-                count_small += 1
-                pair.append(True)
-
-            elif i == '[':
-                count_big += 1
-                pair.append(False)
-
-            elif i == ')':
-                if count_small == 0:
-                    count_small = -1  # 판단을 위한 0이 아닌 임의의 수
-                    break
-                else:
-                    count_small -= 1
-                    if pair.pop() == False:
-                        count_small = -1  # 판단을 위한 0이 아닌 임의의 수
-                        break
-
-            elif i == ']':
-                if count_big == 0:
-                    count_small = -1  # 판단을 위한 0이 아닌 임의의 수
-                    break
-                else:
-                    count_big -= 1
-                    if pair.pop() == True:
-                        count_small = -1  # 판단을 위한 0이 아닌 임의의 수
-                        break
-
-        if count_small == 0 and count_big == 0:
-            print('yes')
-        else:
-            print('no')
+        print("no")
