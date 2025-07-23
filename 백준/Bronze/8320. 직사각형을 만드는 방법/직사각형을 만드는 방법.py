e3750e73b489e13d63bd4822632724ffc485cff2
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-23 21:58:12 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+cnt = 0
+for i in range(1, N+1):
+    for j in range(i, N+1):
+        if i*j <= N:
+            cnt += 1
+        else:
+            break
+
+print(cnt)
