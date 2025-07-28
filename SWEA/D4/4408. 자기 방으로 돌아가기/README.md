2e11ff2ad1e31a4f26d997d4c7433baacaa24358
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-28 18:42:30 +0900

    [D4] Title: 자기 방으로 돌아가기, Time: 95 ms, Memory: 61,440 KB -BaekjoonHub

---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/README.md
@@ -0,0 +1,15 @@
+# [D4] 자기 방으로 돌아가기 - 4408 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWNcJ2sapZMDFAV8) 
+
+### 성능 요약
+
+메모리: 61,440 KB, 시간: 95 ms, 코드길이: 586 Bytes
+
+### 제출 일자
+
+2025-07-28 18:42
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/4408. 자기 방으로 돌아가기/자기 방으로 돌아가기.py
@@ -0,0 +1,21 @@
+'''
+경로가 중복되는 갯수 arr 만들어 해결하기
+'''
+res = []
+
+T = int(input())
+for t in range(1,T+1):
+    N = int(input())
+    cnt_arr = [0]*200
+    for _ in range(N):
+        current_num, target_num = map(int, input().split())
+        # if current_num == target_num:
+        #     continue
+        path_start = (current_num-1)//2
+        path_end = (target_num-1)//2
+        if path_start>path_end:
+            path_start,path_end = path_end,path_start
+        for i in range(path_start,path_end+1):
+            cnt_arr[i] += 1
+    res.append(f"#{t} {max(cnt_arr)}")
+print('\n'.join(res))
