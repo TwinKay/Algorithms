9e45516adc74e84625fc9e888834f4ebfe78ddda
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-29 13:59:55 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 125408 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,56 +1,42 @@
+'''
+아이디어:
+dfs로 풀어보기
+'''
+
 import sys
-sys.setrecursionlimit(((10**5)))
+sys.setrecursionlimit(10000)
 
-for _ in range(int(sys.stdin.readline())):
+delta_x = [0,0,-1,1]
+delta_y = [1,-1,0,0]
 
-    m,n,k = map(int, sys.stdin.readline().split())
+def is_valid(x,y):
+    return 0<=x<M and 0<=y<N
 
+def dfs(x,y):
+    visited[y][x] = True
+    for k in range(4):
+        dx = x + delta_x[k]
+        dy = y + delta_y[k]
+        if is_valid(dx,dy) and graph[dy][dx] == 1 and not visited[dy][dx]: # 범위 안, 배추, 방문 X
+            dfs(dx,dy)
+
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    M,N,K = map(int, sys.stdin.readline().split())
     graph = []
-    for _ in range(n):
-        graph.append([0]*m)
-
-    for _ in range(k):
-        a,b = map(int, sys.stdin.readline().split())
-        graph[b][a] = 1
-
-
-    def dfs(x,y):
-        if x == -1 or y == -1 or x == m+1 or y == n+1:
-            return False
-
-        if graph[y][x] == 1:
-            graph[y][x] = 0
-
-            try:
-                if graph[y][x+1] == 1:
-                    dfs(x+1,y)
-            except:
-                pass
-
-            try:
-                if graph[y][x-1] == 1:
-                    dfs(x-1,y)
-            except:
-                pass
-
-            try:
-                if graph[y+1][x] == 1:
-                    dfs(x,y+1)
-            except:
-                pass
-
-            try:
-                if graph[y-1][x] == 1:
-                    dfs(x,y-1)
-            except:
-                pass
-            return True
-        return False
-
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if dfs(j,i) == True:
-                cnt += 1
-
-    print(cnt)
+    for _ in range(N):
+        graph.append([0]*M)
+    for _ in range(K):
+        x,y = map(int, sys.stdin.readline().split())
+        graph[y][x] = 1
+    visited = []
+    for _ in range(N):
+        visited.append([False]*M)
+    res = 0
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1 and not visited[i][j]: # 배추이고 방문 안한 경우만
+                dfs(j,i)
+                res += 1
+    print(res)
