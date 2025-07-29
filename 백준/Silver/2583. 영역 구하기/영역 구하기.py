528e730eed8794e41fb45493250090c7841c32e3
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-29 14:26:27 +0900

    [Silver I] Title: 영역 구하기, Time: 144 ms, Memory: 130712 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,64 +1,47 @@
-# 상하 반전된 그래프가 나오지만 답은 같음!
+'''
+아이디어:
+dfs로 풀어보기
+boolean 배열로 만들 후, 빈공간 계산
+'''
+
 import sys
-from collections import deque
+sys.setrecursionlimit(10000)
 
-m,n,k = map(int, sys.stdin.readline().split())
+delta_x = [0,0,-1,1]
+delta_y = [1,-1,0,0]
 
+def is_valid(x,y):
+    return 0<=x<M and 0<=y<N
+
+def dfs(x,y):
+    global cnt # 면적 세기 위함
+    visited[y][x] = True
+    for k in range(4):
+        dx = x + delta_x[k]
+        dy = y + delta_y[k]
+        if is_valid(dx,dy) and not graph[dy][dx] and not visited[dy][dx]: # 범위 안, 모눈 종이 x , 방문 x
+            dfs(dx,dy)
+            cnt += 1
+
+N,M,K = map(int, sys.stdin.readline().split())
 graph = []
-for _ in range(m):
-    graph.append([False]*n)
-
-for _ in range(k):
-    x_1,y_1,x_2,y_2 = map(int, sys.stdin.readline().split())
-
-    for i in range(x_1, x_2):
-        for j in range(y_1, y_2):
-            graph[j][i] = True
-
-def bfs(x,y):
-    if graph[y][x] == False:
-        deq = deque([[x,y]])
-        graph[y][x] = True
-        cnt = 1
-        while deq:
-            x,y = deq.popleft()
-
-            if x-1 != -1:
-                if graph[y][x-1] == False:
-                    deq.append([x-1,y])
-                    graph[y][x-1] = True
-                    cnt += 1
-
-            if y-1 != -1:
-                if graph[y-1][x] == False:
-                    deq.append([x,y-1])
-                    graph[y-1][x] = True
-                    cnt += 1
-
-            if x+1 != n:
-                if graph[y][x+1] == False:
-                    deq.append([x+1,y])
-                    graph[y][x+1] = True
-                    cnt += 1
-
-            if y+1 != m:
-                if graph[y+1][x] == False:
-                    deq.append([x,y+1])
-                    graph[y+1][x] = True
-                    cnt += 1
-        return cnt
-
-
-    else:
-        return -1
-
-result = []
-for i in range(m):
-    for j in range(n):
-        ans = bfs(j,i)
-        if ans != -1:
-            result.append(ans)
-result.sort()
-
-print(len(result))
-print(' '.join(map(str, result)))
+for _ in range(N):
+    graph.append([False]*M)
+for _ in range(K):
+    x1,y1,x2,y2 = map(int, sys.stdin.readline().split())
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            graph[i][j] = True # 모눈 종이 겹치기
+visited = []
+for _ in range(N):
+    visited.append([False]*(M))
+res = []
+for i in range(N):
+    for j in range(M):
+        if not graph[i][j] and not visited[i][j]:
+            cnt = 1
+            dfs(j,i)
+            res.append(cnt)
+res.sort()
+print(len(res))
+print(*res)
