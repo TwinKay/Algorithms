e2e991315e2f3aab2b0010a1cb9183a183d71242
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-29 13:50:56 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 121188 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,30 +1,29 @@
+'''
+아이디어:
+dfs로 풀어보기
+양방향 조심!
+visited True 수가 곧 답
+'''
+
 import sys
+sys.setrecursionlimit(10000)
 
-n = int(sys.stdin.readline())
+def dfs(c):
+    visited[c] = True
+    for n in graph[c]:
+        if not visited[n]:
+            dfs(n)
 
-total = []
-for _ in range(int(sys.stdin.readline())):
+V = int(sys.stdin.readline())
+E = int(sys.stdin.readline())
+graph = []
+for _ in range(V+1):
+    graph.append([])
+for _ in range(E):
     a,b = map(int, sys.stdin.readline().split())
-    if b<a:
-        a,b = b,a
-    total.append([a,b])
-total.sort()
+    graph[a].append(b)
+    graph[b].append(a)
+visited = [False]*(V+1)
 
-new_total = []
-for i in total:
-    new_total.append([i[1],i[0]])
-new_total.sort()
-
-check_list = [1]
-for _ in range(100):
-    for i in total:
-        if i[0] in check_list:
-            check_list.append(i[1])
-
-    for j in new_total:
-        if j[0] in check_list:
-            check_list.append(j[1])
-
-
-check_list = list(set(check_list))
-print(len(check_list)-1)
+dfs(1)
+print(sum(visited)-1)
