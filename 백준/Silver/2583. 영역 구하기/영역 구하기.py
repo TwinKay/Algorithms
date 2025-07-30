26bcfe12153dacf5faed5391ec2d0b3aeeae2d4b
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-30 10:18:38 +0900

    [Silver I] Title: 영역 구하기, Time: 136 ms, Memory: 114532 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,27 +1,16 @@
 '''
-아이디어:
-dfs로 풀어보기
-boolean 배열로 만들 후, 빈공간 계산
+BFS를 통한 풀이
+boolean 맵에 겹친 후, False인 것들 그룹 체크
 '''
-
 import sys
-sys.setrecursionlimit(10000)
+from collections import deque
 
-delta_x = [0,0,-1,1]
-delta_y = [1,-1,0,0]
-
+# 가능한 범위 체크 함수
 def is_valid(x,y):
     return 0<=x<M and 0<=y<N
 
-def dfs(x,y):
-    global cnt # 면적 세기 위함
-    visited[y][x] = True
-    for k in range(4):
-        dx = x + delta_x[k]
-        dy = y + delta_y[k]
-        if is_valid(dx,dy) and not graph[dy][dx] and not visited[dy][dx]: # 범위 안, 모눈 종이 x , 방문 x
-            dfs(dx,dy)
-            cnt += 1
+delta_x = [0,0,-1,1]
+delta_y = [1,-1,0,0]
 
 N,M,K = map(int, sys.stdin.readline().split())
 graph = []
@@ -31,17 +20,30 @@ for _ in range(K):
     x1,y1,x2,y2 = map(int, sys.stdin.readline().split())
     for i in range(y1,y2):
         for j in range(x1,x2):
-            graph[i][j] = True # 모눈 종이 겹치기
+            graph[i][j] = True # 계속 겹치기
 visited = []
 for _ in range(N):
     visited.append([False]*(M))
+
 res = []
 for i in range(N):
     for j in range(M):
         if not graph[i][j] and not visited[i][j]:
+            deq = deque()
+            deq.append([j,i])
+            visited[i][j] = True
             cnt = 1
-            dfs(j,i)
+            while deq:
+                cur = deq.popleft()
+                for k in range(4):
+                    dx = cur[0] + delta_x[k]
+                    dy = cur[1] + delta_y[k]
+                    if is_valid(dx,dy) and not graph[dy][dx] and not visited[dy][dx]:
+                        deq.append([dx,dy])
+                        visited[dy][dx] = True
+                        cnt += 1
             res.append(cnt)
+
 res.sort()
 print(len(res))
 print(*res)
