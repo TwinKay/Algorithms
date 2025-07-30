4a0b7d32e93514670c08584bc41ef4c09629d363
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-30 19:35:12 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,20 +1,24 @@
+'''
+실행시간:
+메모리:
+풀이시간: 19:30~
+아이디어: boolean 배열으로 겹치기
+'''
 import sys
 
 graph = []
-for i in range(101):
-    graph.append(list(False for _ in range(101)))
+for _ in range(100):
+    graph.append([False]*100)
 
 N = int(sys.stdin.readline())
 for _ in range(N):
-    x,y = map(int, sys.stdin.readline().split())
-    for i in range(y,y+10):
-        for j in range(x,x+10):
+    a,b = map(int, sys.stdin.readline().split())
+    for i in range(a,a+10):
+        for j in range(b,b+10):
             graph[i][j] = True
-    
-res = 0        
-for line in graph:
-    for bol in line:
-        if bol:
-            res += 1
-            
-print(res)
+cnt = 0
+for i in range(100):
+    for j in range(100):
+        if graph[i][j]:
+            cnt += 1
+print(cnt)
