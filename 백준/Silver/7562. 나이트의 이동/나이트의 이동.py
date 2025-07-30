58f6bae57c1cb4d7ef3cf6d41909bee1b7c97f01
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-30 12:06:10 +0900

    [Silver I] Title: 나이트의 이동, Time: 324 ms, Memory: 127448 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,94 +1,44 @@
+'''
+BFS를 통한 풀이
+8방 탐색
+시작 위치랑 도착 위치가 같을 수 있음
+'''
 import sys
 from collections import deque
 
-for _ in range(int(sys.stdin.readline())):
-    n = int(sys.stdin.readline())
+# 가능한 범위
+def is_valid(x,y):
+    return 0<=x<N and 0<=y<N
 
-    graph = []
-    for _ in range(n):
-        graph.append([0]*n)
+# 8방 탐색
+delta_x = [-2,-1,1,2,-2,-1,1,2]
+delta_y = [-1,-2,-2,-1,1,2,2,1]
 
-    a,b = map(int,sys.stdin.readline().split())
-    x,y = map(int,sys.stdin.readline().split())
+T = int(sys.stdin.readline())
+for t in range(T):
+    N = int(sys.stdin.readline())
+    start_x,start_y = map(int,sys.stdin.readline().split())
+    end_x,end_y = map(int,sys.stdin.readline().split())
 
-    if a == x and b == y:
-        print(0)
+    visited = []
+    for _ in range(N):
+        visited.append([False]*N)
 
-    else:
-        graph[y][x] = -1
-        deq = deque([[a,b]])
-        graph[b][a] += 1    # 움직이지는 않았지만 visited없이 하기 위함
-        while deq:
-            x,y = deq.popleft()
+    deq = deque()
+    deq.append([start_x,start_y,0])
+    visited[start_y][start_x] = True
+    while deq:
+        cur = deq.popleft()
+        time = cur[2]
+        if cur[0] == end_x and cur[1] == end_y:
+            print(time)
+            break
+        for k in range(8):
+            dx = cur[0] + delta_x[k]
+            dy = cur[1] + delta_y[k]
+            if is_valid(dx,dy) and not visited[dy][dx]:
+                deq.append([dx,dy,time+1])
+                visited[dy][dx] = True
 
-            if x-1>=0 and y-2>=0:
-                c = graph[y-2][x-1]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x-1,y-2])
-                    graph[y-2][x-1] = graph[y][x]+1
 
-            if x-2>=0 and y-1>=0:
-                c = graph[y-1][x-2]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x-2,y-1])
-                    graph[y-1][x-2] = graph[y][x]+1
 
-            if x-2>=0 and y+1<=n-1:
-                c = graph[y+1][x-2]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x-2,y+1])
-                    graph[y+1][x-2] = graph[y][x]+1
-
-            if x-1>=0 and y+2<=n-1:
-                c = graph[y+2][x-1]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x-1,y+2])
-                    graph[y+2][x-1] = graph[y][x]+1
-
-            if x+1<=n-1 and y-2>=0:
-                c = graph[y-2][x+1]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x+1,y-2])
-                    graph[y-2][x+1] = graph[y][x]+1
-
-            if x+2<=n-1 and y-1>=0:
-                c = graph[y-1][x+2]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x+2,y-1])
-                    graph[y-1][x+2] = graph[y][x]+1
-
-            if x+2<=n-1 and y+1<=n-1:
-                c = graph[y+1][x+2]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x+2,y+1])
-                    graph[y+1][x+2] = graph[y][x]+1
-
-            if x+1<=n-1 and y+2<=n-1:
-                c = graph[y+2][x+1]
-                if c == -1:
-                    print(graph[y][x])
-                    break
-                if c == 0:
-                    deq.append([x+1,y+2])
-                    graph[y+2][x+1] = graph[y][x]+1
