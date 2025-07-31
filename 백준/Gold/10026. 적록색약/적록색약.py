f70c2d8f65a606a29ff3625c4273bd71410da133
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-31 11:52:04 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 115076 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,67 +1,75 @@
+'''
+아이디어:
+bfs를 통한 풀이
+boolean grpah로 만들어서 쉽게 풀기
+'''
+
 import sys
 from collections import deque
 
-n = int(sys.stdin.readline())
+def is_valid(x,y):
+    return 0<=x<N and 0<=y<N
 
-graph = []
-for _ in range(n):
-    graph.append(list(sys.stdin.readline().rstrip()))
+delta_x = [0,0,-1,1]
+delta_y = [1,-1,0,0]
 
-visited = []
-for _ in range(n):
-    visited.append([False]*n)
+normal_graph = []
+N = int(sys.stdin.readline())
+for _ in range(N):
+    normal_graph.append(list(sys.stdin.readline().rstrip()))
 
-def bfs(x,y):
-    if visited[y][x] == False:
-        c = graph[y][x]
-        deq = deque([[x,y]])
-        visited[y][x] = True
-        while deq:
-            a,b = deq.popleft()
-            # visited[b][a] = True
-
-            if b+1 != n:
-                if visited[b+1][a] == False:
-                    if graph[b+1][a] == c:
-                        deq.append([a,b+1])
-                        visited[b+1][a] = True
-            if b-1 != -1:
-                if visited[b-1][a] == False:
-                    if graph[b-1][a] == c:
-                        deq.append([a,b-1])
-                        visited[b-1][a] = True
-
-            if a+1 != n:
-                if visited[b][a+1] == False:
-                    if graph[b][a+1] == c:
-                        deq.append([a+1,b])
-                        visited[b][a+1] = True
-
-            if a-1 != -1:
-                if visited[b][a-1] == False:
-                    if graph[b][a-1] == c:
-                        deq.append([a-1,b])
-                        visited[b][a-1] = True
-        return 1
-    return 0
-
-cnt_1 = 0
-for i in range(n):
-    for j in range(n):
-        cnt_1 += bfs(j,i)
-
-for i in range(n):
-    for j in range(n):
-        if graph[j][i] == 'R':
-            graph[j][i] = 'G'
+blind_graph = []
+for i in range(N):
+    blind_graph.append([])
+    for j in range(N):
+        if normal_graph[i][j] == 'B':
+            blind_graph[i].append(False)
+        else:
+            blind_graph[i].append(True)
 
 visited = []
-for _ in range(n):
-    visited.append([False]*n)
+for _ in range(N):
+    visited.append([False]*N)
 
-cnt_2 = 0
-for i in range(n):
-    for j in range(n):
-        cnt_2 += bfs(j,i)
+cnt_normal = 0
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            cnt_normal += 1
 
-print(cnt_1, cnt_2)
+            deq = deque()
+            deq.append([j,i])
+            visited[i][j] = True
+            while deq:
+                cur = deq.popleft()
+                for k in range(4):
+                    dx = cur[0] + delta_x[k]
+                    dy = cur[1] + delta_y[k]
+                    if is_valid(dx,dy) and normal_graph[dy][dx] == normal_graph[i][j] and not visited[dy][dx]:
+                        deq.append([dx,dy])
+                        visited[dy][dx] = True
+
+# 새로 초기화 필수
+visited = []
+for _ in range(N):
+    visited.append([False]*N)
+
+cnt_blind = 0
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            cnt_blind += 1
+
+            deq = deque()
+            deq.append([j,i])
+            visited[i][j] = True
+            while deq:
+                cur = deq.popleft()
+                for k in range(4):
+                    dx = cur[0] + delta_x[k]
+                    dy = cur[1] + delta_y[k]
+                    if is_valid(dx,dy) and blind_graph[dy][dx] == blind_graph[i][j] and not visited[dy][dx]:
+                        deq.append([dx,dy])
+                        visited[dy][dx] = True
+
+print(cnt_normal,cnt_blind)
