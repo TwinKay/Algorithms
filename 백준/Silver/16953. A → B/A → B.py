0252974e245f840fb1562e39842af823968bf8be
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-07-31 09:41:19 +0900

    [Silver II] Title: A → B, Time: 124 ms, Memory: 112604 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,23 +1,34 @@
+'''
+아이디어:
+bfs를 통한 풀이
+범위는 B까지만 확인
+만들 수 없다면 -1
+string 연산(1을 오른쪽 추가)보다는 *10+1로 효율
+메모리 초과 조심 -> visited 배열 필요가 없네..?
+'''
+
 import sys
 from collections import deque
 
-a,b = map(int, sys.stdin.readline().split())
+A,B = map(int, sys.stdin.readline().split())
 
-result = []
-deq = deque([[a,0]])
+deq = deque()
+deq.append([A,1]) # num, 연산 횟수
+res = -1 # 답 없다면 그대로 -1
 while deq:
-    a,cnt = deq.popleft()
+    cur = deq.popleft()
+    num = cur[0]
+    cnt = cur[1]
 
-    if a>b:
-        pass
-    elif a==b:
-        result.append(cnt)
-    else:
-        cnt += 1
-        deq.append([a*2,cnt])
-        deq.append([a*10+1,cnt])
+    if num == B: # B 만나면
+        res = cnt
+        break
 
-if len(result) == 0:
-    print(-1)
-else:
-    print(min(result)+1)
+    n1 = num*2
+    n2 = num*10 + 1 # 오른쪽 1추가
+    if n1 <= B:
+        deq.append([n1,cnt+1])
+    if n2 <= B:
+        deq.append([n2,cnt+1])
+
+print(res)
