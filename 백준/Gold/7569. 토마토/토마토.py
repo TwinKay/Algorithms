d199452f74c72dbe4893f6fe771fdb7affbe607d
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-01 09:45:26 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 209284 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,85 +1,66 @@
+'''
+토마토는 없을 수도 있음
+다 익는 시간 or 없으면 -1
+2based time 사용해서 한 arr에 저장
+한번에 deq 넣기
+'''
 import sys
 from collections import deque
 
-m,n,h = map(int, sys.stdin.readline().split())
+delta_x = [0,0,-1,1,0,0]
+delta_y = [-1,1,0,0,0,0]
+delta_h = [0,0,0,0,1,-1]
 
+M,N,H = map(int, sys.stdin.readline().split())
 graph = []
-for _ in range(h):
+for h in range(H):
     graph.append([])
+    for i in range(N):
+        graph[h].append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(h):
-    for j in range(n):
-        graph[i].append(list(map(int, sys.stdin.readline().split())))
+def is_valid(x,y,z):
+    return 0<=x<M and 0<=y<N and 0<=z<H
 
-visited = []
-for _ in range(h):
-    visited.append([])
+deq = deque()
+for h in range(H):
+    for i in range(N):
+        for j in range(M):
+            if graph[h][i][j] == 1: # 익은 토마토
+                deq.append([j,i,h,1])
 
-for i in range(h):
-    for j in range(n):
-        visited[i].append([0]*m)
+while deq:
+    cur = deq.popleft()
+    cx = cur[0]
+    cy = cur[1]
+    ch = cur[2]
+    time = cur[3]
+    for k in range(6):
+        dx = cx + delta_x[k]
+        dy = cy + delta_y[k]
+        dh = ch + delta_h[k]
+        if is_valid(dx,dy,dh) and graph[dh][dy][dx] == 0:
+            deq.append([dx,dy,dh,time+1])
+            graph[dh][dy][dx] = time + 1
 
-def bfs():
-    deq = deque([])
-    for z,i in enumerate(graph):
-        for y,j in enumerate(i):
-            for x,k in enumerate(j):
-                if k == 1:
-                    deq.append([x,y,z])
-                    visited[z][y][x] = 1    # 결과값에서 1빼주기
-    while deq:
-        x,y,z = deq.popleft()
+flag = False
+max_val = 1
+for h in range(H):
+    if flag:
+        break
+    for i in range(N):
+        if flag:
+            break
+        for j in range(M):
+            if flag:
+                break
+            val = graph[h][i][j]
+            if val == 0:
+                flag= True
+                break
+            elif val >= 2:
+                max_val = max(max_val,val)
 
-        if x+1 != m:
-            if visited[z][y][x+1] == 0 and graph[z][y][x+1] == 0:
-                deq.append([x+1,y,z])
-                visited[z][y][x+1] = visited[z][y][x] + 1
-                graph[z][y][x+1] = 1
-
-        if x != 0:
-            if visited[z][y][x-1] == 0 and graph[z][y][x-1] == 0:
-                deq.append([x-1,y,z])
-                visited[z][y][x-1] = visited[z][y][x] + 1
-                graph[z][y][x-1] = 1
-
-        if y+1 != n:
-            if visited[z][y+1][x] == 0 and graph[z][y+1][x] == 0:
-                deq.append([x,y+1,z])
-                visited[z][y+1][x] = visited[z][y][x] + 1
-                graph[z][y+1][x] = 1
-
-        if y != 0:
-            if visited[z][y-1][x] == 0 and graph[z][y-1][x] == 0:
-                deq.append([x,y-1,z])
-                visited[z][y-1][x] = visited[z][y][x] + 1
-                graph[z][y-1][x] = 1
-
-        if z+1 != h:
-            if visited[z+1][y][x] == 0 and graph[z+1][y][x] == 0:
-                deq.append([x,y,z+1])
-                visited[z+1][y][x] = visited[z][y][x] + 1
-                graph[z+1][y][x] = 1
-
-        if z != 0:
-            if visited[z-1][y][x] == 0 and graph[z-1][y][x] == 0:
-                deq.append([x,y,z-1])
-                visited[z-1][y][x] = visited[z][y][x] + 1
-                graph[z-1][y][x] = 1
-
-
-bfs()
-
-graph_1 = []
-for i in graph:
-    for j in i:
-        graph_1.extend(j)
-
-visited_1 = []
-for i in visited:
-    for j in i:
-        visited_1.extend(j)
-
-if 0 in graph_1:
+if flag:
     print(-1)
 else:
-    print(max(visited_1)-1)
+    print(max_val-1) # 시간을 2부터 기록했음으로
