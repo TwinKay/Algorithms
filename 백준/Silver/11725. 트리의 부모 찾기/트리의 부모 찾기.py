e9d1c3266692adba05abaec957ba781bd7a025a8
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-04 16:06:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 320 ms, Memory: 310424 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,33 +1,31 @@
+'''
+DFS...? 결국 DFS도 재귀니깐,,?
+'''
+
 import sys
-from collections import deque
+sys.setrecursionlimit(10**5)
 
-n = int(sys.stdin.readline())
+def recu(c):
+    if visited[c]:
+        return
+    for n in graph[c]:
+        if not visited[n]:
+            res[n] = c
+            visited[c] = True
+            recu(n)
 
+N = int(sys.stdin.readline())
 graph = []
-for _ in range(n+1):
+for _ in range(N+1):
     graph.append([])
+visited = [False]*(N+1)
 
-for _ in range(n-1):
-    a,b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+for _ in range(N-1):
+    node_a, node_b = map(int, sys.stdin.readline().split())
+    graph[node_a].append(node_b) # 이어진 부분 연결
+    graph[node_b].append(node_a)
 
-for i in graph:
-    i.sort()
-
-visited = [False]*(n+1)
-result = [0]*(n+1)
-
-
-deq = deque([1])
-while deq:
-    a = deq.popleft()
-    result.append(a)
-    visited[a] = True
-    for i in graph[a]:
-        if not visited[i]:
-            result[i] += a
-            deq.append(i)
-
-for i in result[2:n+1]:
-    print(i)
+res = [0]*(N+1)
+recu(1)
+for r in res[2:]:
+    print(r)
