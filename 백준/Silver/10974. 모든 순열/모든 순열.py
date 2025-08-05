0f75d18f28991f1f9b1439de212308c6791af496
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-05 11:49:04 +0900

    [Silver III] Title: 모든 순열, Time: 172 ms, Memory: 114040 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,9 +1,18 @@
+'''
+아이디어:
+백트래킹
+'''
 import sys
-from itertools import permutations
 
-n = int(sys.stdin.readline())
+def back_tracking(n,res):
+    if n==N:
+        print(*res)
+        return
+    for i in range(1,N+1):
+        if i not in res:
+            res.append(i)
+            back_tracking(n+1,res)
+            res.pop()
 
-arr = list(range(1,n+1))
-l =list(permutations(arr,n))
-for i in l:
-    print(' '.join(list(map(str,i))))
+N = int(sys.stdin.readline())
+back_tracking(0,[])
