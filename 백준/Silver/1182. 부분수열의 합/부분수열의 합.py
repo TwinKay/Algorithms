aa95553acdbd0697fb20206620ac7d07e2adadd1
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-05 12:17:29 +0900

    [Silver II] Title: 부분수열의 합, Time: 792 ms, Memory: 115708 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,27 +1,25 @@
 '''
 아이디어:
 백트래킹
++ sum으로 매개변수 넘기기
 '''
 import sys
 
-def back_tracking(n,start,res):
+def back_tracking(n,start,sm):
     global cnt
     if n==m:
-        sm = 0
-        for r in res: # 원소값 더하기
-            sm += arr[r]
         if sm == S:
             cnt += 1
         return
     for i in range(start,N):
-        res.append(i)
-        back_tracking(n+1,i+1,res)
-        res.pop()
+        sm += arr[i]
+        back_tracking(n+1,i+1,sm)
+        sm -= arr[i]
 
 N,S = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 cnt = 0
 for m in range(1,N+1):
-    back_tracking(0,0,[])
+    back_tracking(0,0,0)
 
 print(cnt)
