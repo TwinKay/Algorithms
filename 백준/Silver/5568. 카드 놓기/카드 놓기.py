6e424f482f25856a99e22d66c449da4eb6f82c6b
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-06 11:23:06 +0900

    [Silver IV] Title: 카드 놓기, Time: 100 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -1,25 +1,29 @@
+'''
+아이디어:
+입력을 String 자체롤 받은 후, nPr 수행하며 join후에 set에 넣기
+'''
 import sys
 
-N = int(sys.stdin.readline())
-K = int(sys.stdin.readline())
-arr = list()
-for _ in range(N):
-    arr.append(sys.stdin.readline().rstrip())
-
-s = set()
-
-def perm(res_arr,K,visited):
-    if len(res_arr) == K:
-        s.add(int("".join(res_arr)))
+def back_tracking(n,cand):
+    if n == K: # 기저
+        s.add("".join(cand))
         return
     for i in range(N):
         if not visited[i]:
-            res_arr.append(arr[i])
             visited[i] = True
-            perm(res_arr,K,visited)
+            cand.append(arr[i])
+            back_tracking(n+1,cand)
             visited[i] = False
-            res_arr.pop()
+            cand.pop()
 
-perm([],K,[False]*N)
 
+N = int(sys.stdin.readline())
+K = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+    arr.append(sys.stdin.readline().rstrip())
+
+s = set() # 정수 담을 set
+visited = [False]*N
+back_tracking(0,[]) # n, 조합 담을 arr
 print(len(s))
