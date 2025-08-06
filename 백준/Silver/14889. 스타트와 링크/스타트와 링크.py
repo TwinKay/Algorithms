729f96200efd12e90d4c945352c6adca1c04aa61
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-06 17:09:02 +0900

    [Silver I] Title: 스타트와 링크, Time: 772 ms, Memory: 113800 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -13,30 +13,27 @@ for _ in range(N):
 
 def back_tracking(n,start,team1):
     global min_val
-    if n == N//2: # 기저 조건
-        team1 = set(team1)
+    if n == half_N: # 기저 조건
         team2 = [] # 다른 팀 조합 만들기
         for i in range(N):
             if i not in team1:
                 team2.append(i)
-
-        team1 = list(team1)
+                
         team1_overall = 0
-        for i in range(N//2): # i==j는 0이라 괜찮다 -> 조건 추가가 시간 더 들듯
-            for j in range(N//2):
+        for i in range(half_N): # i==j는 0이라 괜찮다 -> 조건 추가가 시간 더 들듯
+            for j in range(half_N):
                 team1_overall += arr[team1[i]][team1[j]]
         team2_overall = 0
-        for i in range(N//2):
-            for j in range(N//2):
+        for i in range(half_N):
+            for j in range(half_N):
                 team2_overall += arr[team2[i]][team2[j]]
         min_val = min(min_val,abs(team1_overall-team2_overall))
         return
 
     for i in range(start,N):
-        team1.append(i)
-        back_tracking(n+1,i+1,team1)
-        team1.pop()
+        back_tracking(n+1,i+1,team1+[i])
 
+half_N = N//2
 min_val = float('inf')
 back_tracking(0,0,[])
 print(min_val)
