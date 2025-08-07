e2638302d3c82e6de19addb3b6110371786b4b80
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-07 10:33:16 +0900

    [Bronze II] Title: 수 정렬하기, Time: 116 ms, Memory: 237872 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,9 +1,37 @@
+'''
+아이디어:
+퀵 소트 구현
+'''
 import sys
+sys.setrecursionlimit(10**5)
+
+def quick_sort(arr, left, right):
+    if left >= right: # left랑 right가 같아지면 return
+        return
+
+    pivot = arr[left]
+    i = left + 1 # pivot 제외
+    j = right
+
+    while i <= j:
+        while i <= right and arr[i] <= pivot:
+            i += 1
+        while j >= left + 1 and arr[j] >= pivot:
+            j -= 1
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i] # 값 바꿔주기
+
+    arr[left], arr[j] = arr[j], arr[left] # pivot 넣기
+
+    # 재귀
+    quick_sort(arr, left, j - 1)
+    quick_sort(arr, j + 1, right)
 
 N = int(sys.stdin.readline())
-lst = []
+arr = []
 for _ in range(N):
-    lst.append(int(sys.stdin.readline()))
-lst.sort()
-for i in lst:
-    print(i)
+    arr.append(int(sys.stdin.readline()))
+quick_sort(arr, 0, len(arr)-1)
+
+for a in arr:
+    print(a)
