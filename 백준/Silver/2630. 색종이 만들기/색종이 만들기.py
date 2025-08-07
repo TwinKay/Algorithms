2a3d2dcfceda220cfe1329ce2153696bc1a2660a
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-07 11:56:00 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 112080 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,35 +1,35 @@
+'''
+아이디어:
+분할 정복 -> N//2하면서 분할하기
+첫번째 color와 다른 순간에 분할
+모두 같다면 같은 color에 cnt++
+'''
 import sys
 
-n = int(sys.stdin.readline())
-
-paper = []
-for _ in range(n):
-    paper.append(list(map(int, sys.stdin.readline().split())))
-
-w=0
-b=0
-
-def cut(x,y,n):
-    global w,b
-    dst = set()
-    for i in range(x,x+n):
-        for j in range(y,y+n):
-            dst.add(paper[i][j])
-
-    if len(dst) != 1:
-        cut(x, y, n//2)
-        cut(x, y + n//2, n//2)
-        cut(x + n//2, y, n//2)
-        cut(x + n//2, y + n//2, n//2)
-        return
-
+def recu(start_x,start_y,n):
+    global cnt_blue,cnt_white
+    color = graph[start_y][start_x] # 기준 color 선정
+    for i in range(n):
+        for j in range(n):
+            x = start_x + j
+            y = start_y + i
+            if graph[y][x] != color: # 기준 color와 다르면 재귀 호출 후 return
+                recu(start_x,start_y,n//2)
+                recu(start_x+n//2,start_y,n//2)
+                recu(start_x,start_y+n//2,n//2)
+                recu(start_x+n//2,start_y+n//2,n//2)
+                return
+    if color:
+        cnt_blue += 1
     else:
-        dst = list(dst)
-        if dst[0] == 0:
-            w += 1
-        else:
-            b += 1
+        cnt_white += 1
+        
+N = int(sys.stdin.readline())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
-cut(0,0,n)
-print(w)
-print(b)
+cnt_blue = cnt_white = 0
+recu(0,0,N)
+print(cnt_white)
+print(cnt_blue)
