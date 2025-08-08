0c706d747311acf81308894b556625b5cc8e0470
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-08 10:50:14 +0900

    [Silver II] Title: 나무 자르기, Time: 448 ms, Memory: 258356 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,27 +1,44 @@
-# 뭔가 이상한데,, 이분탐색 더 공부해야지..
+'''
+아이디어:
+이진탐색 이용
+답은 원하는 길이와 같지 않을 수 있음(초과)
+'''
 import sys
 
-def cut(x):
-    cnt = 0
-    for i in tree:
-        if i-x > 0:
-            cnt += i-x
-    return cnt
+def get_tree_length(height):
+    '''
+    특정 높이에서 나무를 잘랐을 때 얻는 나무 길이를 반환하는 함수
+    :param (int) height: 절단기의 높이
+    :return: (int) 얻은 나무 길이
+    '''
+    total_length = 0
+    for a in arr:
+        total_length += max(0,a-height)
 
-n,m = map(int, sys.stdin.readline().split())
-tree = list(map(int, sys.stdin.readline().split()))
+    return total_length
 
-d, u = 0, max(tree)
-while d < u-1:
 
-    b = (d+u)//2
+def b_search(left,right,target_length):
+    '''
+    이진탐색 함수
+    :param left: (int) left index (최소 높이)
+    :param right: (int) right index (최대 높이)
+    :param target_length: (int) 필요한 나무 길이
+    :return: (int) 나무를 절단하는데 필요한 최대 높이
+    '''
+    result = 0
+    while left <= right:
+        mid = (left+right)//2
+        tree_length = get_tree_length(mid)
 
-    if cut(b) >= m:
-        d = b
-    else:
-        u = b
-    
-if cut(u) >= m:
-    print(u)
-else:
-    print(d)
+        if tree_length >= target_length:
+            result = mid
+            left = mid+1
+        else:
+            right = mid-1
+    return result
+
+
+N,M = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+print(b_search(0,max(arr),M)) # max(arr)부터 탐색 가능
