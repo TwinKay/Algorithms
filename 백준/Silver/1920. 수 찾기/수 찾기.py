6a41596fffb5c2e66fb4664f82d9316001cf4af3
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-08 09:58:45 +0900

    [Silver IV] Title: 수 찾기, Time: 204 ms, Memory: 136936 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,32 @@
-import sys
-
-n = int(sys.stdin.readline())
-main = list(map(int, sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-case = list(map(int, sys.stdin.readline().split()))
-main.sort()
-
-def binary(main_list, case_i):
-    start = 0 ; end = n-1
-    while start <= end:
-        mid = (start+end)//2
-        if main_list[mid] == case_i:
+'''
+아이디어:
+이진탐색 이용
+주의: 정렬 안 된 arr 입력
+'''
+def b_search(left,right,target):
+    '''
+    이진탐색 함수
+    :param left: (int) left index
+    :param right: (int) right index
+    :return: (int) 값이 있으면 1
+    '''
+    while True:
+        if left > right:
+            return 0
+        mid = (left+right)//2
+        if arr[mid] == target:
             return 1
-        elif main_list[mid] > case_i:
-            end = mid - 1
+        elif arr[mid] > target:
+            right = mid-1
         else:
-            start = mid + 1
-    return 0
+            left = mid+1
 
-for i in range(m):
-    if binary(main, case[i]) == 1:
-        print(1)
-    else:
-        print(0)
+N = int(input())
+arr = list(map(int, input().split()))
+arr.sort() # 이진탐색에서는 정렬 필수
+
+Q = int(input())
+queries = list(map(int, input().split()))
+for query in queries:
+    print(b_search(0,N-1,query))
+
