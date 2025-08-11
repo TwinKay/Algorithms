5e7bd98afda5f7e6e564beac42960f818fc6916a
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-11 16:56:50 +0900

    [Silver III] Title: 계단 오르기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,22 +1,24 @@
+'''
+아이디어:
+DP를 통한 해결
+'''
 import sys
 
-n = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
+arr = []
+for _ in range(N):
+    arr.append(int(sys.stdin.readline()))
 
-stairs = []
-for _ in range(n):
-    stairs.append(int(sys.stdin.readline()))
-
-result = []
-
-if n == 1:
-    print(stairs[0])
-elif n == 2:
-    print(stairs[0]+stairs[1])
+if N==1:
+    print(arr[0])
+elif N==2:
+    print(arr[0]+arr[1])
 else:
-    result.append(stairs[0])
-    result.append(stairs[0]+stairs[1])
-    result.append(max(stairs[0]+stairs[2], stairs[1]+stairs[2]))
-    for i in range(3,n):
-        result.append(max(result[i-2]+stairs[i], result[i-3]+stairs[i-1]+stairs[i]))
+    # dp 테이블 초기화
+    dp = [0]*300
+    dp[0] = arr[0]
+    dp[1] = arr[0] + arr[1]
+    for i in range(2,N):
+        dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i]) # 3연속 피하기 위한 점화식
 
-    print(result.pop())
+    print(dp[N-1])
