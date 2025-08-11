8ff0b90965083eae86c80f33bca22cb81caf1f08
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-11 15:55:17 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,17 @@
+'''
+아이디어:
+DP를 통한 해결 -> n^2이지만 가능
+'''
 import sys
 
-n = int(sys.stdin.readline())
-l = list(map(int, sys.stdin.readline().split()))
-dp = [1]*n
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
 
-for i in range(n):
+dp = [1]*N # dp 테이블 초기화
+
+for i in range(N):
     for j in range(i):
-        if l[i] > l[j]:
-            dp[i] = max(dp[j]+1, dp[i])
-            
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i],dp[j]+1) # 갱신
+
 print(max(dp))
