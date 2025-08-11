81fc0c441e2077cd5f644f8a74c53c0ebb5d40a9
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-11 13:30:17 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,7 +1,15 @@
-import sys
-n = int(sys.stdin.readline())
-result = [1,3]
-for i in range(n-2):
-    result.append(result[i]*2+result[i+1])
+'''
+아이디어:
+점화식 발견을 통한 DP 사용하기
+'''
 
-print(result[n-1]%10007)
+import sys
+
+N = int(sys.stdin.readline())
+dp = [0]*(1001) # dp table 초기화
+dp[1] = 1
+dp[2] = 3
+for i in range(3,N+1):
+    dp[i] = (dp[i-2]*2 + dp[i-1])%10_007 # 점화식
+
+print(dp[N])
