7b72925f3dd909f9c84da788d99bf8e7b83aa3bb
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-11 12:13:20 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,25 @@
+'''
+아이디어:
+DP 사용하기
+각 정수마다 가능한 연산끼리 min
+'''
+
 import sys
 
-n = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
 
-result = [0]*(n+1)
-for i in range(2,n+1):
-    result[i] = result[i-1]+1
+dp = [0]*(N+1) # 초기 dp 테이블 생성
 
-    if i%2 == 0:
-        result[i] = min(result[i//2]+1, result[i])
-    if i%3 == 0:
-        result[i] = min(result[i//3]+1, result[i])
+num = 1
+dp[num] = 0
+for i in range(2,N+1):
+    if i % 3 == 0 and i % 2 == 0: # 나누어 떨어지는 경우
+        dp[i] = min(dp[i//3]+1,dp[i//2]+1,dp[i-1]+1)
+    elif i % 3 == 0:
+        dp[i] = min(dp[i//3]+1,dp[i-1]+1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i // 2] + 1, dp[i - 1] + 1)
+    else:
+        dp[i] = dp[i-1]+1
 
-print(result[n])
+print(dp[N])
