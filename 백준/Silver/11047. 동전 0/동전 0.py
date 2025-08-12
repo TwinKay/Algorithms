486e2e3d6b37f1377747301d609a11c71931e9da
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-12 09:23:26 +0900

    [Silver IV] Title: 동전 0, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,13 +1,17 @@
+'''
+아이디어:
+greedy로 풀이
+'''
 import sys
 
-N, K = map(int, sys.stdin.readline().split())
-coins = [int(sys.stdin.readline()) for _ in range(N)]
-coins.sort(reverse=True)
+N,K = map(int, sys.stdin.readline().split())
+coins = []
+for _ in range(N):
+    coins.append(int(sys.stdin.readline()))
 
 cnt = 0
-for coin in coins:
-    if K >= coin:
-        cnt += K // coin
-        K %= coin
+for coin in coins[::-1]:
+    cnt += K//coin # coin 단위로 가능한만큼
+    K %= coin # 나누어 떨어지지 못한만큼 유지
 
 print(cnt)
