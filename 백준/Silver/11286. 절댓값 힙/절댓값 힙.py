c244309b94a9f53d4d430c26970ba2042c899bb5
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-12 13:57:36 +0900

    [Silver I] Title: 절댓값 힙, Time: 188 ms, Memory: 116520 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,14 +1,20 @@
+'''
+아이디어:
+튜플을 통한 abs heap 구현
++ 음수가 더 높은 순위
+'''
 import sys
-import heapq
+from heapq import heappush,heappop
 
-heap = []
-for _ in range(int(sys.stdin.readline())):
-    x = int(sys.stdin.readline())
-    if x == 0:
-        if len(heap) == 0:
+N = int(sys.stdin.readline())
+pq = []
+for _ in range(N):
+    query = int(sys.stdin.readline())
+    if query != 0: # 출력하는 경우
+        heappush(pq, (abs(query),query)) # 절대값, 동일 시 자동으로 음수부터
+    else:
+        if len(pq) == 0:
             print(0)
         else:
-            print(heapq.heappop(heap)[1])
+            print(heappop(pq)[1])
 
-    else:
-        heapq.heappush(heap, ((x**2)**0.5,x))
