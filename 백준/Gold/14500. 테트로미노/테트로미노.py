febe3299be5bb3122c2ebd4e66272ef73aaf2a98
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-19 15:24:39 +0900

    [Gold IV] Title: 테트로미노, Time: 300 ms, Memory: 113108 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,106 +1,50 @@
+'''
+dfs로 할 걸.. 하다가 생각남..ㅠㅠ
+'''
+
 import sys
 
-n,m = map(int, sys.stdin.readline().split())
+def is_valid(x,y):
+    return 0<=x<M and 0<=y<N
 
+tetro_idxs = [
+    [(0,0),(1,0),(2,0),(3,0)],
+    [(0,0),(0,1),(0,2),(0,3)],
+    [(0,0),(0,1),(1,0),(1,1)],
+    [(0,0),(0,1),(0,2),(1,2)],
+    [(0,2),(1,2),(1,1),(1,0)],
+    [(0,0),(1,0),(2,0),(2,1)],
+    [(0,0),(0,1),(1,1),(2,1)],
+    [(0,0), (0,1), (0,2), (1,0)],
+    [(0,0), (1,0), (1,1), (1,2)],
+    [(2,0), (2,1), (1,1), (0,1)],
+    [(0,0), (1,0), (2,0), (0,1)],
+    [(0,1), (1,1), (1,0), (2,0)],
+    [(0,0), (1,0), (1,1), (2,1)],
+    [(0,0), (0,1), (1,1), (1,2)],
+    [(1,0), (1,1), (0,1), (0,2)],
+    [(0,0), (1,0), (2,0), (1,1)],
+    [(0, 1), (1, 1), (2, 1), (1,0)],
+    [(0,0), (0,1), (0,2), (1,1)],
+    [(1,0), (1,1), (1,2), (0,1)],
+]
+
+max_val = -1
+N,M = map(int, sys.stdin.readline().split())
 graph = []
-for i in range(n):
+for _ in range(N):
     graph.append(list(map(int, sys.stdin.readline().split())))
+for i in range(N):
+    for j in range(M):
+        for tetro_idx in tetro_idxs:
+            cnt = 0
+            for idx in tetro_idx:
+                x = j + idx[0]
+                y = i + idx[1]
+                if not is_valid(x,y):
+                    break
+                cnt += graph[y][x]
+            else:
+                max_val = max(max_val,cnt)
 
-result = []
-
-#1
-for i in range(n-3):
-    for j in range(m):
-        result.append(graph[i][j]+graph[i+1][j]+graph[i+2][j]+graph[i+3][j])
-
-#2
-for i in range(n):
-    for j in range(m-3):
-        result.append(sum(graph[i][j:j+4]))
-
-#3
-for i in range(n-1):
-    for j in range(m-1):
-        result.append(sum(graph[i][j:j+2])+sum(graph[i+1][j:j+2]))
-
-#4
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j]+graph[i+1][j]+sum(graph[i+2][j:j+2]))
-
-#5
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(sum(graph[i][j:j+3])+graph[i+1][j])
-
-#6
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(sum(graph[i][j:j+2])+graph[i+1][j+1]+graph[i+2][j+1])
-
-#7
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(graph[i][j+2]+sum(graph[i+1][j:j+3]))
-
-#8
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j+1]+graph[i+1][j+1]+sum(graph[i+2][j:j+2]))
-
-#9
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(sum(graph[i][j:j+3])+graph[i+1][j+2])
-
-#10
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(sum(graph[i][j:j+2])+graph[i+1][j]+graph[i+2][j])
-
-#11
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(graph[i][j]+sum(graph[i+1][j:j+3]))
-
-#12
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j]+sum(graph[i+1][j:j+2])+graph[i+2][j+1])
-
-#13
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j+1]+sum(graph[i+1][j:j+2])+graph[i+2][j])
-
-#14
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(sum(graph[i][j+1:j+3])+sum(graph[i+1][j:j+2]))
-
-#15
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(sum(graph[i][j:j+2])+sum(graph[i+1][j+1:j+3]))
-
-#16
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(sum(graph[i][j:j+3])+graph[i+1][j+1])
-
-#17
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j+1]+sum(graph[i+1][j:j+2])+graph[i+2][j+1])
-
-#18
-for i in range(n-1):
-    for j in range(m-2):
-        result.append(graph[i][j+1]+sum(graph[i+1][j:j+3]))
-
-#19
-for i in range(n-2):
-    for j in range(m-1):
-        result.append(graph[i][j]+sum(graph[i+1][j:j+2])+graph[i+2][j])
-
-print(max(result))
+print(max_val)
