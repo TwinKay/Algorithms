9732c9a164b6604f9f0996e6146cb49a5c0675d0
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-21 16:04:23 +0900

    [Gold IV] Title: 연구소, Time: 360 ms, Memory: 114372 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,3 +1,6 @@
+'''
+최적화해보기
+'''
 import sys
 from itertools import combinations
 from collections import deque
@@ -13,55 +16,51 @@ graph = []
 for _ in range(N):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
-empty_idxs = []
-virus_idxs = []
+first_visited = []
+for _ in range(N):
+    first_visited.append([False] * M)
+
+wall_num = 0 # 기존 벽 갯수
+empty_idxs = [] # 0인 index 넣을 리스트
+virus_idxs = [] # 바이러스 index 넣을 리스트
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 0:
             empty_idxs.append([j,i])
+        elif graph[i][j] == 1:
+            wall_num += 1
         elif graph[i][j] == 2:
             virus_idxs.append([j,i])
             graph[i][j] = 0
+            first_visited[i][j] = True # 바이러스는 미리 visited 처리해서 최적화, 나중에 조합마다 딥카피
 
-min_virus = float('inf')
-max_safe = -1
-for wall_idxs in combinations(empty_idxs,3):
+min_virus = float('inf') # 가장 적은 바이러스 -> 가장 큰 안전여ㅕㅇ역
+for wall_idxs in combinations(empty_idxs,3): # 벽을 세울 수 있는 조합들
+    # 벽 세우기
     graph[wall_idxs[0][1]][wall_idxs[0][0]] = 1
     graph[wall_idxs[1][1]][wall_idxs[1][0]] = 1
     graph[wall_idxs[2][1]][wall_idxs[2][0]] = 1
 
+    visited = [sub[:] for sub in first_visited] # 기존 바이러스 visited 처리된 visited 딥카피(슬라이싱)
 
-    visited = []
-    for _ in range(N):
-        visited.append([False]*M)
+    deq = deque(virus_idxs) # 기존 바이러스 index 리스트 넣어서 뎈 생성
 
-    deq = deque(virus_idxs)
-    for v_idx in virus_idxs:
-        visited[v_idx[1]][v_idx[0]] = True
-
-    cnt = 0
+    cnt = 0 # 바이러스 count
     while deq:
         x,y = deq.popleft()
         cnt += 1
         for k in range(4):
             dx = x + delta_x[k]
             dy = y + delta_y[k]
-            if is_valid(dx,dy) and not visited[dy][dx] and graph[dy][dx] == 0:
+            if is_valid(dx,dy) and not visited[dy][dx] and graph[dy][dx] == 0: # 가능 범위, 방문 X, 빈 공간
                 deq.append([dx,dy])
                 visited[dy][dx] = True
 
-    min_virus = min(min_virus,cnt)
+    min_virus = min(min_virus,cnt) # 갱신
 
-    safe = 0
-    for i in range(N):
-        for j in range(M):
-            if not visited[i][j] and graph[i][j] == 0:
-                safe += 1
-    max_safe = max(max_safe,safe)
-
-
+    # 다음 조합을 위해 벽 빼주기
     graph[wall_idxs[0][1]][wall_idxs[0][0]] = 0
     graph[wall_idxs[1][1]][wall_idxs[1][0]] = 0
     graph[wall_idxs[2][1]][wall_idxs[2][0]] = 0
 
-print(max_safe)
+print(N*M-wall_num-3-min_virus) # 전체 - 기존 벽 갯수 - 새로운 벽 3개 - 최소 바이러스
