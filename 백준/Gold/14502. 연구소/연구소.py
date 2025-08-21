d237cc02a12f2eba6e51e40a0dfc2259d5eb6cff
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-21 15:39:19 +0900

    [Gold IV] Title: 연구소, Time: 368 ms, Memory: 114888 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,67 +1,67 @@
-import sys, copy
+import sys
+from itertools import combinations
 from collections import deque
 
-n,m = map(int, sys.stdin.readline().split())
+def is_valid(x,y):
+    return 0<=x<M and 0<=y<N
 
+delta_x = [0,0,-1,1]
+delta_y = [-1,1,0,0]
+
+N,M = map(int, sys.stdin.readline().split())
 graph = []
-for _ in range(n):
+for _ in range(N):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
-wall = []
-for y,i in enumerate(graph):
-    for x,j in enumerate(i):
-        if j == 0:
-            wall.append([x,y])
+empty_idxs = []
+virus_idxs = []
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            empty_idxs.append([j,i])
+        elif graph[i][j] == 2:
+            virus_idxs.append([j,i])
+            graph[i][j] = 0
 
-def bfs(a,b,c):
-    graph_copy = copy.deepcopy(graph)
-    x_1, y_1 = wall[a]
-    x_2, y_2 = wall[b]
-    x_3, y_3 = wall[c]
-    graph_copy[y_1][x_1] = 1
-    graph_copy[y_2][x_2] = 1
-    graph_copy[y_3][x_3] = 1
+min_virus = float('inf')
+max_safe = -1
+for wall_idxs in combinations(empty_idxs,3):
+    graph[wall_idxs[0][1]][wall_idxs[0][0]] = 1
+    graph[wall_idxs[1][1]][wall_idxs[1][0]] = 1
+    graph[wall_idxs[2][1]][wall_idxs[2][0]] = 1
 
-    for b in range(n):
-        for a in range(m):
-            if graph_copy[b][a] == 2:
-                deq = deque([[a,b]])
 
-                while deq:
-                    x,y = deq.popleft()
+    visited = []
+    for _ in range(N):
+        visited.append([False]*M)
 
-                    if x-1 != -1:
-                        if graph_copy[y][x-1] == 0:
-                            graph_copy[y][x-1] = 2
-                            deq.append([x-1,y])
-
-                    if y-1 != -1:
-                        if graph_copy[y-1][x] == 0:
-                            graph_copy[y-1][x] = 2
-                            deq.append([x,y-1])
-
-                    if x+1 != m:
-                        if graph_copy[y][x+1] == 0:
-                            graph_copy[y][x+1] = 2
-                            deq.append([x+1,y])
-
-                    if y+1 != n:
-                        if graph_copy[y+1][x] == 0:
-                            graph_copy[y+1][x] = 2
-                            deq.append([x,y+1])
+    deq = deque(virus_idxs)
+    for v_idx in virus_idxs:
+        visited[v_idx[1]][v_idx[0]] = True
 
     cnt = 0
-    for i in graph_copy:
-        for j in i:
-            if j == 0:
-                cnt += 1
-    return cnt
+    while deq:
+        x,y = deq.popleft()
+        cnt += 1
+        for k in range(4):
+            dx = x + delta_x[k]
+            dy = y + delta_y[k]
+            if is_valid(dx,dy) and not visited[dy][dx] and graph[dy][dx] == 0:
+                deq.append([dx,dy])
+                visited[dy][dx] = True
 
-result = []
-for i in range(len(wall)):
-    for j in range(len(wall)):
-        for k in range(len(wall)):
-            if i < j < k:
-                result.append(bfs(i,j,k))
+    min_virus = min(min_virus,cnt)
 
-print(max(result))
+    safe = 0
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j] and graph[i][j] == 0:
+                safe += 1
+    max_safe = max(max_safe,safe)
+
+
+    graph[wall_idxs[0][1]][wall_idxs[0][0]] = 0
+    graph[wall_idxs[1][1]][wall_idxs[1][0]] = 0
+    graph[wall_idxs[2][1]][wall_idxs[2][0]] = 0
+
+print(max_safe)
