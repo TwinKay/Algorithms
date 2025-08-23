1198c629a6b8e7eedfe8ab1ce725486b22693819
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-23 10:47:44 +0900

    [Gold IV] Title: 뱀, Time: 124 ms, Memory: 114452 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -11,6 +11,12 @@ def is_valid(x,y):
     return 0<=x<N and 0<=y<N
 
 def is_can_go(x,y):
+    '''
+    뱀이 갈 수 있으면 True를 반환하는 함수
+    :param x: (int) 뱀이 이동할 x
+    :param y: (int) 뱀이 이동할 y
+    :return: (boolean) True -> 이동 가능
+    '''
     if is_valid(x,y):
         if (x,y) not in snake_idxs:
             return True
@@ -36,32 +42,32 @@ snake_idxs.add((0,0))
 
 
 L = int(sys.stdin.readline())
-timeline = {}
+timeline = {} # 시간의 끝을 알 수도 없으며 list에서 조회한다면 시간 오래 걸림
 for _ in range(L):
     t,direct = sys.stdin.readline().split()
-    timeline[int(t)] = direct
+    timeline[int(t)] = direct # timeline에 key:time value:방향 으로 저장
 
 
-direct = 0
+direct = 0 # 위
 time = 0
 while True:
     time += 1
     head_x,head_y = snake[0]
     nx = head_x + delta_x[direct%4]
     ny = head_y + delta_y[direct%4]
-    if not is_can_go(nx,ny):
+    if not is_can_go(nx,ny): # 본인 또는 벽과 만나는 경우
         break
 
-    snake.appendleft((nx,ny))
-    snake_idxs.add((nx,ny))
+    snake.appendleft((nx,ny)) # 머리를 늘려 이동
+    snake_idxs.add((nx,ny)) # set에도 관리
 
-    if graph[ny][nx] == 0:
-        pop_idx = snake.pop()
-        snake_idxs.remove(pop_idx)
-    else:
-        graph[ny][nx] = 0
+    if graph[ny][nx] == 0: # 사과 없음
+        pop_idx = snake.pop() # 꼬리 자르기
+        snake_idxs.remove(pop_idx) # 꼬리 set에도 관리
+    else: # 사과 있음 (꼬리 자르기 X)
+        graph[ny][nx] = 0 # 사과 먹기
 
-    if time in timeline:
+    if time in timeline: # 회전을 해야하는 시간이면
         rotate_direct = timeline[time]
         if rotate_direct == 'L':
             direct -= 1
