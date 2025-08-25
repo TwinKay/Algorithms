67f306fd6bdc56bde63689247d1fc4055b544ff9
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-08-25 15:42:55 +0900

    [Gold IV] Title: 인구 이동, Time: 816 ms, Memory: 121324 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,56 +1,52 @@
 import sys
 from collections import deque
 
+def is_valid(x,y):
+    return 0<=x<N and 0<=y<N
+
+delta_x = [0,0,-1,1]
+delta_y = [1,-1,0,0]
+
 N,L,R = map(int, sys.stdin.readline().split())
 
 graph = []
 for _ in range(N):
     graph.append(list(map(int, sys.stdin.readline().split())))
 
-delta_x = [0,0,-1,1]
-delta_y = [1,-1,0,0]
-
-def is_valid(x,y):
-    return x>=0 and x<N and y>=0 and y<N
-
-def move():
+time = -1
+while True:
+    time += 1
+    is_change = False
     visited = []
     for _ in range(N):
         visited.append([False]*N)
-
-    is_move = False
-
     for i in range(N):
         for j in range(N):
             if not visited[i][j]:
+                new_group_idxs = []
+                new_group_sm = 0
                 deq = deque()
-                deq.append([j,i])
+                deq.append((j,i))
                 visited[i][j] = True
-                union = [[j,i]]
                 while deq:
                     cur = deq.popleft()
+                    x,y = cur
+                    new_group_idxs.append(cur)
+                    new_group_sm += graph[y][x]
                     for k in range(4):
-                        dx = cur[0] + delta_x[k]
-                        dy = cur[1] + delta_y[k]
-                        if is_valid(dx,dy) and not visited[dy][dx]:
-                            diff = abs(graph[cur[1]][cur[0]] - graph[dy][dx])
-                            if L <= diff <= R:
-                                deq.append([dx,dy])
-                                visited[dy][dx] = True
-                                union.append([dx,dy])
-                                is_move = True
-                union_sum = 0
-                for u in union:
-                    union_sum += graph[u[1]][u[0]]
+                        dx = x + delta_x[k]
+                        dy = y + delta_y[k]
+                        if is_valid(dx,dy) and not visited[dy][dx] and L<=abs(graph[y][x]-graph[dy][dx])<=R:
+                            deq.append((dx,dy))
+                            visited[dy][dx] = True
+                if len(new_group_idxs) >= 2:
+                    is_change = True
+                    aver = new_group_sm//len(new_group_idxs)
+                    for new_group_idx in new_group_idxs:
+                        graph[new_group_idx[1]][new_group_idx[0]] = aver
 
-                aver = union_sum//len(union)
-                for u in union:
-                    graph[u[1]][u[0]] = aver
+    if not is_change:
+        break
+    is_change = False
 
-    return is_move
-
-cnt = 0
-while move():
-    cnt += 1
-
-print(cnt)
+print(time)
