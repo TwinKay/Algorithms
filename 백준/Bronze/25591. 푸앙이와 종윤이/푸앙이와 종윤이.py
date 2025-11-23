a65e79d259e1fa080939c06fcbda13d7d278b937
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-11-24 01:58:49 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.py
@@ -0,0 +1,15 @@
+x, y = map(int, input().split())
+
+a = 100 - x
+b = 100 - y
+
+c = 100 - (a + b)
+d = a * b
+
+q, r = divmod(d, 100)
+
+front = c + q
+back = r
+
+print(a, b, c, d, q, r)
+print(front, back)
