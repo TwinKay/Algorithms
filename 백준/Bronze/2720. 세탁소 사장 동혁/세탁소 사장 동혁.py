b835b615574380b0b48dbbd39ff8aab2d8615ec4
고금강 <101968683+TwinKay@users.noreply.github.com>
2025-12-17 23:36:54 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 112 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    c = int(sys.stdin.readline())
+
+    q = c // 25
+    c %= 25
+
+    d = c // 10
+    c %= 10
+
+    n = c // 5
+    c %= 5
+
+    p = c
+
+    print(q, d, n, p)
