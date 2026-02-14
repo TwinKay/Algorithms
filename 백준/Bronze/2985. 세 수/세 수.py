20863aeed60caf322fb0384b65429d8a2479ebe2
고금강 <101968683+TwinKay@users.noreply.github.com>
2026-02-14 23:16:39 +0900

    [Bronze III] Title: 세 수, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.py
@@ -0,0 +1,18 @@
+A, B, C = map(int, input().split())
+
+if A + B == C:
+    print(f"{A}+{B}={C}")
+elif A - B == C:
+    print(f"{A}-{B}={C}")
+elif A * B == C:
+    print(f"{A}*{B}={C}")
+elif B != 0 and A % B == 0 and A // B == C:
+    print(f"{A}/{B}={C}")
+elif B + C == A:
+    print(f"{A}={B}+{C}")
+elif B - C == A:
+    print(f"{A}={B}-{C}")
+elif B * C == A:
+    print(f"{A}={B}*{C}")
+elif C != 0 and B % C == 0 and B // C == A:
+    print(f"{A}={B}/{C}")
